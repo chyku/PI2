aa2447aab604a37904afcbc947fbc025516a8149
chyku <ky.cu303@gmail.com>
2018-02-22 10:25:53 -0500

    Realtime Update

--- graph.py
+++ graph.py
@@ -3,9 +3,6 @@
 """x = np.linspace(0, 6*np.pi, 100)
 y = np.sin(x)
 
-# You probably won't need this if you're embedding things in a tkinter plot...
-plt.ion()
-
 fig = plt.figure()
 ax = fig.add_subplot(111)
 line1, = ax.plot(x, y, 'r-') # Returns a tuple of line objects, thus the comma
@@ -13,6 +10,8 @@ line1, = ax.plot(x, y, 'r-') # Returns a tuple of line objects, thus the comma
 for phase in np.linspace(0, 10*np.pi, 500):
     line1.set_ydata(np.sin(x + phase))
     fig.canvas.draw()
+
+    set_ydata
 """
 
 import matplotlib.pyplot as plt; plt.rcdefaults()
@@ -25,11 +24,12 @@ occupancy = [85,80,60]
  
 plt.barh(y_pos, occupancy, align='center', alpha=.5)
 plt.yticks(y_pos, objects)
-plt.xticks(y_pos, objects)
+plt.xticks(np.arange(0, 100, 25))
 plt.xlim(0, 100)
 plt.xlabel('Occupancy')
 plt.ylabel('Car Number')
 plt.title('Train Occupancy')
 
+# (graphname?).set_ydata(numpy.append(hl.get_xdata(), new_data))
 
 plt.show()
